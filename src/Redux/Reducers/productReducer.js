ad6d206d605bcfa0617e223967b774f5704e8cf7
Daksh902 <146697073+Daksh902@users.noreply.github.com>
2024-06-09 14:56:59 +0530

    Update productReducer.js

--- src/Redux/Reducers/productReducer.js
+++ src/Redux/Reducers/productReducer.js
@@ -1,37 +1,27 @@
-
-// for creating slice and AsycnThunk
-import { createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+// for creating slice and Async Thunk
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 // firebase database
-import {db} from "../../firebaseInit";
+import { db } from "../../firebaseInit";
 import { updateDoc, doc, arrayUnion, onSnapshot, arrayRemove } from "firebase/firestore";
 
-// for toast notificatioins
+// for toast notifications
 import { toast } from "react-toastify";
 
-
-
 // return date in yy/mm/dd format
-function getDate(){
-    // getting current date
+function getDate() {
     const date = new Date();
-    // day 
     let day = date.getDate();
-    // month
     let month = date.getMonth() + 1;
-    // year
     let year = date.getFullYear();
-    // yy/mm/dd format
-    return(`${year}-${month}-${day}`);
+    return (`${year}-${month}-${day}`);
 }
 
-
-
-// async thunk to get initial Data of cart items and order placed by user from database
+// async thunk to get initial data of cart items and orders placed by user from database
 export const getInitialCartOrdersThunk = createAsyncThunk(
     'product/getCartOrders',
     (args, thunkAPI) => {
-        const { authReducer, productReducer } = thunkAPI.getState();
+        const { authReducer } = thunkAPI.getState();
         const { isLoggedIn, userLoggedIn } = authReducer;
 
         if (isLoggedIn) {
@@ -45,300 +35,217 @@ export const getInitialCartOrdersThunk = createAsyncThunk(
     }
 );
 
-
-
 // async thunk to update cart inside the database
 const updateCartInDatabase = createAsyncThunk(
     'product/updateCartInDatabase',
-    async(args,thunkAPI) => {
-
-        // user's data from initialState
-        const {authReducer, productReducer} = thunkAPI.getState();
+    async (args, thunkAPI) => {
+        const { authReducer, productReducer } = thunkAPI.getState();
         const { userLoggedIn } = authReducer;
 
-        // update the cart inside the firebase database
         const userRef = doc(db, "buybusy-redux", userLoggedIn.id);
         await updateDoc(userRef, {
             cart: productReducer.cart
         });
     }
-)
-
-
-
-// increase product quantity in cart and database
-export const increaseQuantThunk = createAsyncThunk(
-    "product/increaseProductQuantity",
-    async (product,thunkAPI) => {
-        // user's data
-        const {productReducer} = thunkAPI.getState();
-
-        // finding product inside the cart
-        const index=productReducer.cart.findIndex((item) => item.name === product.name);
-        
-        // increase the quantity of product in initialState cart
-        thunkAPI.dispatch(increaseProductQuantity(index));      
-        
-        // increase total amount of products in cart
-        thunkAPI.dispatch(increaseTotalAmount(product.price));
-
-        // update the cart inside the database
-        thunkAPI.dispatch(updateCartInDatabase());
-    }
-)
-
-
-// decrease the quantity of product in cart and database
-export const decreaseQuantThunk = createAsyncThunk(
-    "product/decreaseProductQuantity",
-    async(product,thunkAPI) => {
-
-        // getting user's data 
-        const { productReducer } = thunkAPI.getState();
-        
-        // finding the product inside the cart 
-        const index=productReducer.cart.findIndex((item) => item.name === product.name);
-        
-        // if quantity of product is 1 then remove it from the cart
-        if(productReducer.cart[index].quantity === 1){
-            thunkAPI.dispatch(removeFromCartThunk(product));
-            return;
-        }
-
-        // else
-        // decrease the quantity of product inside the cart
-        thunkAPI.dispatch(decreaseProductQuantity(index));
-
-        // reduce the amount of product from total amount of cart
-        thunkAPI.dispatch(reduceTotalAmount(productReducer.cart[index].price));
-
-        // update the cart inside the database
-        thunkAPI.dispatch(updateCartInDatabase());
-        
-    }
-)
-
+);
 
 // async thunk for adding a new product to the cart
 export const addToCartThunk = createAsyncThunk(
     "product/addToCart",
-    async (product,thunkAPI) => {
-        // getting user's data 
-        const { authReducer,productReducer } = thunkAPI.getState();
-        const {isLoggedIn,userLoggedIn} = authReducer;
-        
-        // check whether user is logged in or not
-        if(!isLoggedIn){
+    async (product, thunkAPI) => {
+        const { authReducer, productReducer } = thunkAPI.getState();
+        const { isLoggedIn, userLoggedIn } = authReducer;
+
+        if (!isLoggedIn) {
             toast.error("Please first Login !!!");
             return;
         }
 
-        // checking whether the product already in the cart
-        const index=productReducer.cart.findIndex((item) => item.name === product.name);
-        if(index !== -1){
-            // if product already in cart then increase quantity and return
+        const index = productReducer.cart.findIndex((item) => item.name === product.name);
+        if (index !== -1) {
             thunkAPI.dispatch(increaseQuantThunk(productReducer.cart[index]));
             toast.success("Product Quantity Increased!!");
             return;
         }
 
-        // else
-        // add product to the cart of loggedIn user in database
         const userRef = doc(db, "buybusy-redux", userLoggedIn.id);
         await updateDoc(userRef, {
-            cart: arrayUnion({quantity:1,...product})
+            cart: arrayUnion({ quantity: 1, ...product })
         });
-        
-        // increase total amount and product quantity
+
         thunkAPI.dispatch(increaseTotalAmount(product.price));
         thunkAPI.dispatch(increaseTotalItem());
 
-        // notification
+        thunkAPI.dispatch(updateCartInDatabase()); // Ensure the database is updated
+
         toast.success("Added to your Cart!!");
     }
 );
 
+// increase product quantity in cart and database
+export const increaseQuantThunk = createAsyncThunk(
+    "product/increaseProductQuantity",
+    async (product, thunkAPI) => {
+        const { productReducer } = thunkAPI.getState();
+        const index = productReducer.cart.findIndex((item) => item.name === product.name);
+
+        thunkAPI.dispatch(increaseProductQuantity(index));      
+        thunkAPI.dispatch(increaseTotalAmount(product.price));
+        thunkAPI.dispatch(updateCartInDatabase());
+    }
+);
+
+// decrease product quantity in cart and database
+export const decreaseQuantThunk = createAsyncThunk(
+    "product/decreaseProductQuantity",
+    async (product, thunkAPI) => {
+        const { productReducer } = thunkAPI.getState();
+        const index = productReducer.cart.findIndex((item) => item.name === product.name);
+
+        if (productReducer.cart[index].quantity === 1) {
+            thunkAPI.dispatch(removeFromCartThunk(product));
+            return;
+        }
+
+        thunkAPI.dispatch(decreaseProductQuantity(index));
+        thunkAPI.dispatch(reduceTotalAmount(productReducer.cart[index].price));
+        thunkAPI.dispatch(updateCartInDatabase());
+    }
+);
 
 // remove product from the cart
 export const removeFromCartThunk = createAsyncThunk(
     "product/removeFromCart",
-    async(product,thunkAPI) => {
-
-        // getting user's data
+    async (product, thunkAPI) => {
         const { authReducer } = thunkAPI.getState();
-        const {userLoggedIn} = authReducer;
-        
-        // remove the product from cart in database 
+        const { userLoggedIn } = authReducer;
+
         const userRef = doc(db, "buybusy-redux", userLoggedIn.id);
         await updateDoc(userRef, {
             cart: arrayRemove(product)
         });
 
-        // returning product to the extraReducer for further operations
         return product;
     }
 );
 
-
 // async thunk to remove all the products from the cart
 export const clearCartThunk = createAsyncThunk(
     "product/emptyCart",
-    async (args,thunkAPI) => {
-
-        // getting user's data
+    async (args, thunkAPI) => {
         const { authReducer, productReducer } = thunkAPI.getState();
-        const {userLoggedIn} = authReducer;
-        
-        // if no item in cart then return with message
-        if(productReducer.itemInCart === 0){
-            toast.error("Nothing to remove in Cart!!");    
+        const { userLoggedIn } = authReducer;
+
+        if (productReducer.itemInCart === 0) {
+            toast.error("Nothing to remove in Cart!!");
             return;
         }
 
-        // empty cart array in database
         const userRef = doc(db, "buybusy-redux", userLoggedIn.id);
         await updateDoc(userRef, {
             cart: []
         });
 
-        // notification
         toast.success("Empty Cart!!");
     }
 );
 
-
 // async thunk to purchase all the product inside the cart
 export const purchaseAllThunk = createAsyncThunk(
     "product/purchaseAllItems",
-    async (args,thunkAPI) => {
+    async (args, thunkAPI) => {
+        const { authReducer, productReducer } = thunkAPI.getState();
+        const { userLoggedIn } = authReducer;
 
-        // getting user's data 
-        const { authReducer,productReducer} = thunkAPI.getState();
-        const {userLoggedIn} = authReducer;
-        
-        // get current data from function
-        const currentDate=getDate();
-        
-        // adding order to database with data, products and amount
+        const currentDate = getDate();
+
         const userRef = doc(db, "buybusy-redux", userLoggedIn.id);
         await updateDoc(userRef, {
-            orders: arrayUnion({date:currentDate,
-                                list:productReducer.cart,
-                                amount:productReducer.total})
-            }
-        );
+            orders: arrayUnion({
+                date: currentDate,
+                list: productReducer.cart,
+                amount: productReducer.total
+            })
+        });
 
-        // removng all the product's from cart
         thunkAPI.dispatch(clearCartThunk());
     }
 );
 
-
-
-// slice to manage operation of products
 const productSlice = createSlice({
-    name:"product",
-    // containing cart for products, total number of products in side the cart, list of all the order placed by the user, total amount of the products inside the cart
-    initialState:{
-        cart:[],
-        itemInCart:0,
-        myorders:[],
-        total:0,
+    name: "product",
+    initialState: {
+        cart: [],
+        itemInCart: 0,
+        myorders: [],
+        total: 0,
     },
-    reducers:{
-        // to initialize the myorder array on first render
-        setMyOrders: (state,action) => {
+    reducers: {
+        setMyOrders: (state, action) => {
             state.myorders = action.payload;
-            return;
         },
-        // increase quantity of a product in cart
-        increaseProductQuantity: (state,action) => {
+        increaseProductQuantity: (state, action) => {
             const index = action.payload;
-            state.cart.at(index).quantity++;
-            return; 
+            state.cart[index].quantity++;
         },
-        // decrease quantity of a product in cart
-        decreaseProductQuantity: (state,action) => {
+        decreaseProductQuantity: (state, action) => {
             const index = action.payload;
-            state.cart.at(index).quantity--;
-            return;
+            state.cart[index].quantity--;
         },
-        // initialize the cart array on first render
-        setCart: (state,action) => {
+        setCart: (state, action) => {
             state.cart = action.payload;
-            return;
         },
-        // to increase the total number of items in cart
-        increaseTotalItem: (state,action) => {
+        increaseTotalItem: (state) => {
             state.itemInCart++;
-            return;
         },
-        // to increase the total amount of products inside the cart
-        increaseTotalAmount: (state,action) => {
+        increaseTotalAmount: (state, action) => {
             state.total += action.payload;
-            return;
         },
-        // to decrease the total amount of products inside the cart
-        reduceTotalAmount: (state,action) => {
+        reduceTotalAmount: (state, action) => {
             state.total -= action.payload;
-            return;
         }
     },
     extraReducers: (builder) => {
-        // update the state after getting data from database
-        builder.addCase(getInitialCartOrdersThunk.fulfilled, (state,action) => {
-            const cart = action.payload;
-            if(cart){    
-                let sum=0,len =0;
-                cart.map((item) => {
-                    Number(sum += item.price * item.quantity);
-                    Number(len += item.quantity);
-                });
-                state.total = sum;
-                state.itemInCart = len;
-            }
-        })
-        // update state after increasing product quantity in cart and database
-        .addCase(increaseQuantThunk.fulfilled, (state,action) => {
-            state.itemInCart++;
-        })
-        // update state after decreasing product quantity in cart and database
-        .addCase(decreaseQuantThunk.fulfilled, (state,action) => {
-            if(state.itemInCart > 1){
-                state.itemInCart--;
-            }
-        })
-        // update state after removing product from cart and database
-        .addCase(removeFromCartThunk.fulfilled, (state,action) => {
-            const product = action.payload;
-            // reduce item count and total amount
-            state.total -= product.quantity * product.price;
-            state.itemInCart -= product.quantity;
-            // notification
-            toast.success("Removed from Cart!!");
-        })
-        // update state after removing all products from cart
-        .addCase(clearCartThunk.fulfilled, (state,action) => {
-            state.itemInCart = 0;
-            state.total = 0;
-            state.cart=[];
-        })
+        builder
+            .addCase(getInitialCartOrdersThunk.fulfilled, (state, action) => {
+                const cart = action.payload;
+                if (cart) {
+                    let sum = 0, len = 0;
+                    cart.forEach((item) => {
+                        sum += item.price * item.quantity;
+                        len += item.quantity;
+                    });
+                    state.total = sum;
+                    state.itemInCart = len;
+                }
+            })
+            .addCase(increaseQuantThunk.fulfilled, (state) => {
+                state.itemInCart++;
+            })
+            .addCase(decreaseQuantThunk.fulfilled, (state) => {
+                if (state.itemInCart > 1) {
+                    state.itemInCart--;
+                }
+            })
+            .addCase(removeFromCartThunk.fulfilled, (state, action) => {
+                const product = action.payload;
+                state.total -= product.quantity * product.price;
+                state.itemInCart -= product.quantity;
+                toast.success("Removed from Cart!!");
+            })
+            .addCase(clearCartThunk.fulfilled, (state) => {
+                state.itemInCart = 0;
+                state.total = 0;
+                state.cart = [];
+            });
     }
 });
-
 
 // exporting the reducer of slice
 export const productReducer = productSlice.reducer;
 
 // exporting all the actions of reducer
-export const { setMyOrders, 
-            increaseProductQuantity, 
-            decreaseProductQuantity, 
-            setCart, 
-            increaseTotalItem,
-            increaseTotalAmount, 
-            reduceTotalAmount } = productSlice.actions;
-
+export const { setMyOrders, increaseProductQuantity, decreaseProductQuantity, setCart, increaseTotalItem, increaseTotalAmount, reduceTotalAmount } = productSlice.actions;
 
 // exporting the state of reducer to get data
 export const productSelector = (state) => state.productReducer;
